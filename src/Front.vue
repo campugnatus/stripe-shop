0496d11c0bdf4b26b9b36055e4fa49f4f0c0a666
Andrey Shevchuk <campugnatus@gmail.com>
2022-01-18 18:42:24 +0300

    cart menu items padding

--- src/Front.vue
+++ src/Front.vue
@@ -41,7 +41,7 @@
             <div class="pt-2">menu</div>
           </button>
         </section>
-        <section class="space-x-7 flex">
+        <section class="space-x-4 md:space-x-7 flex items-center">
           <button class="lg:p-2 text-sm lg:text-base hidden sm:inline">
             <div class="flex justify-center">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 lg:h-11 lg:w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -57,7 +57,7 @@
             </svg>            </div>
             <div class="pt-2">log in</div>
           </button>
-          <router-link class="lg:p-2 text-sm lg:text-base" to="/cart">
+          <router-link class="sm:inline lg:p-2 text-sm lg:text-base" to="/cart">
             <div class="flex justify-center">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 lg:h-11 lg:w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
