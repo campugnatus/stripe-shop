1025e4b90943b00ed0842fb9fc7ae88b52294b10
Andrey Shevchuk <campugnatus@gmail.com>
2022-01-27 01:24:17 +0300

    header refactoring

--- src/components/ShopHeader.vue
+++ src/components/ShopHeader.vue
@@ -1,99 +1,52 @@
 <template>
-  <header class="hidden lg:flex px-6 lg:px-8 container mx-auto mt-14 items-center">
-    <!-- logo -->
-    <router-link to="/" class="font-sigmar">
-      <div class="text-4xl tracking-[6px] mb-[-4px]">
-        STRIPE
-      </div>
-      <div class="text-5xl tracking-[10px]">
-        SHOP
-      </div>
-    </router-link>
+  <header class="grid grid-cols-2 gap-3 lg:flex px-3 lg:px-6 lg:px-8 lg:container mx-auto mt-2 lg:mt-14 items-center" style="">
 
-    <div class="flex flex-col justify-between ml-10 xl:ml-20 flex-shrink-0 font-roboto uppercase">
+    <section class="mt-2 lg:mt-0">
+      <router-link to="/" class="font-sigmar">
+        <div class="text-[26px] lg:text-4xl tracking-[5px] lg:tracking-[6px] mb-[-16px] lg:mb-[-4px]">
+          STRIPE
+        </div>
+        <div class="text-[34px] lg:text-5xl tracking-[8px]">
+          SHOP
+        </div>
+      </router-link>
+    </section>
+
+    <section class="row-start-2 col-span-2 flex lg:flex-col lg:justify-between lg:ml-10 xl:ml-20 flex-shrink-0 font-roboto uppercase space-x-4 lg:space-x-0">
       <router-link class="hover:text-blue-600" to="/catalogue">catalogue</router-link>
       <router-link class="hover:text-blue-600" to="#">about</router-link>
       <router-link class="hover:text-blue-600" to="#">third one</router-link>
-    </div>
+    </section>
 
-    <div class="mx-16 xl:mx-32 flex-grow flex items-center">
+    <!-- search box -->
+    <section class="row-start-3 col-span-2 lg:mx-16 xl:mx-32 flex-grow flex items-center">
       <div class="relative w-full">
-        
-        <!-- search box -->
-        <input type="text" name="search" placeholder="Search for stripes" class="text-lg border border-gray-300 rounded-full p-2 pl-5 pr-14 w-full focus:outline-none focus:border-gray-600">
+        <input type="text" name="search" placeholder="Search for stripes" class="lg:text-lg border border-gray-300 rounded lg:rounded-full pl-3 lg:pl-5 pr-14 w-full focus:outline-none focus:border-gray-600">
         <div class="absolute right-0 inset-y-0 mr-4 h-full flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" class="scale-x-[-1] text-gray-400 h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
         </div>
       </div>
-    </div>
+    </section>
 
-    <div class="space-x- flex flex-shrink-0">
-      <button class="lg:p-2 text-sm lg:text-base hidden sm:inline">
+    <section class="row-start-1 col-start-2 flex justify-end flex-shrink-0 lg:space-x-4">
+      <button class="text-sm lg:text-base">
         <div class="flex justify-center w-20">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 lg:h-11 lg:w-11 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 lg:h-11 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
           </svg>            
         </div>
-        <div class="pt-2 text-center">Log in</div>
+        <div class="lg:pt-2 text-center">Log in</div>
       </button>
+
       <CartButton/>
-    </div>
-  </header>
-
-  <header class="lg:hidden px-4 sm:px-6 max-w-screen-lg mx-auto mt-6">
-    <div class="flex justify-between items-center">
-      <router-link to="/" class="font-sigmar">
-        <div class="text-3xl tracking-[5px] mb-[-8px]">
-          STRIPE
-        </div>
-        <div class="text-4xl tracking-[10px]">
-          SHOP
-        </div>
-      </router-link>
-
-      <div class="flex">
-        <button class="text-sm mr-5">
-          <div class="flex justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 lg:h-11 lg:w-11 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-          </svg>            </div>
-          <div class="pt-2">Log in</div>
-        </button>
-        <CartButton/>
-      </div>
-
-      <div class="hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </div>
-    </div>
-    <div class="mt-3 space-x-7">
-      <router-link class="font-roboto leading-snug" to="/catalogue">catalogue </router-link>
-      <router-link class="font-roboto leading-snug" to="#">about </router-link>
-      <router-link class="font-roboto leading-snug" to="#">third one</router-link>
-    </div>
-    <div class="w-full mt-4">
-      <div class="relative w-full">
-        <input type="text" name="search" placeholder="Search for stripes" class="border border-gray-300 rounded-full p-2 pl-5 pr-14 w-full focus:outline-none focus:border-gray-600">
-        <div class="absolute right-0 inset-y-0 mr-4 h-full flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="scale-x-[-1] text-gray-400 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg>
-        </div>
-      </div>
-    </div>
+    </section>
   </header>
 </template>
 
 <script setup>
 import CartButton from '@/components/CartButton.vue'
-import { useCartStore } from '@/stores/cart'
-import { computed } from 'vue'
-
-const cart = useCartStore()
 </script>
 
 <style>
