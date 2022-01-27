870192415d4f55ce5bfd5840ce8f73ae7e3e3ceb
Andrey Shevchuk <campugnatus@gmail.com>
2022-01-27 16:41:45 +0300

    prevent clicks from propagation on carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="relative">
+
 		<button v-show="showLeftShadow" class="w-16 h-16 rounded-full border-4 ring-inset ring-1 ring-gray-200 border-white flex justify-center items-center absolute left-2 -translate-y-1/2 top-1/2 bg-gray-100 hover:bg-gray-200 z-20">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
 			</svg>
 		</button>
+
 		<button v-show="showRightShadow" class="w-16 h-16 rounded-full border-4 ring-inset ring-1 ring-gray-200 border-white flex justify-center items-center absolute right-2 -translate-y-1/2 top-1/2 bg-gray-100 hover:bg-gray-200 z-20">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M14 5l7 7m0 0l-7 7m7-7H3" />
 			</svg>
 		</button>	
-		<div class="relative overflow-hidden" @mousedown="mousedown" @mouseup="mouseup" @mousemove="mousemove" @mouseleave="mouseleave">
+
+		<div class="relative overflow-hidden" @mousedown="mousedown" @mouseup="mouseup" @mousemove="mousemove" @mouseleave="mouseleave" @click.capture="onClick">
 			<div v-show="showLeftShadow" class="h-full w-5 -ml-5 absolute left-0 shadow-xl z-10 border-r-4 border-red-300"></div>
 			<div class="scrollbar-off flex gap-5 w-full overflow-x-auto relative px-5" ref="carousel" @scroll="onscroll">
 				<!-- <div class=""></div> -->
@@ -38,7 +41,9 @@ import WareCard from './WareCard.vue'
 import {ref, onMounted} from 'vue';
 
 // true= user is holding the carousel with their mouse
-var holding = false;
+let holding = false;
+// true= user has moved the carousel
+let moved = false;
 
 const carousel = ref(null)
 var zeroX;
@@ -51,9 +56,13 @@ var showRightShadow = ref(false);
 // sure how to do it properly
 const listMargin = 21; 
 
+
 onMounted(onscroll);
 
 function mousedown (e) {
+	moved = false
+	e.preventDefault();
+	e.stopPropagation();
 	holding = true;
 	zeroX = e.pageX;
 	zeroScroll = carousel.value.scrollLeft;
@@ -69,6 +78,7 @@ function mouseleave (e) {
 
 function mousemove (e) {
 	if (!holding) return;
+	moved = true;
 	e.preventDefault();
 	const el = carousel.value;
 	var zeroOffset = zeroX - el.offsetLeft;
@@ -84,7 +94,10 @@ function onscroll(e) {
 	showRightShadow.value = el.scrollLeft < scrollLength - listMargin;
 }
 
-
+function onClick (evt) {
+	if (moved)
+		evt.preventDefault();
+}
 
 
 
@@ -95,12 +108,14 @@ function onscroll(e) {
 // Just using the browser's built in scrolling seems to have some benefits.
 // 1) it's already there. Why do anything else?
 // 2) Some mobile nice-to-haves by default:
-//    * Device tries to distinguish between vertical and horizontal scroll, not doing both simultaneously
+//    * Device tries to distinguish between vertical and horizontal scroll,
+//      not doing both simultaneously
 //    * Some elasticity on the edges?
 //
 // Scrolling drawbacks:
-// 1) You can only control scroll position through JS, not through CSS (although you can set margin on the element to set the 
-//    initial position in a way)
+// 1) You can only control scroll position through JS, not through CSS
+//    (although you can set margin on the element to set the initial position in
+//    a way)
 // 2) scrollbar-off is a bit hacky (webkit-scrollbar, wtf)
 
 </script>
