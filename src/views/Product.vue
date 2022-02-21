c1fc7301d7add2872b4b8e5f97d96721fd3bec31
Andrey Shevchuk <campugnatus@gmail.com>
2022-02-21 15:21:07 +0300

    product view loading state

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,66 +1,67 @@
 <template>
 	<ShopHeader/>
 
-	<main class="mx-auto mt-20 lg:mt-32 grid md:grid-cols-2 gap-8 lg:gap-16 px-4 sm:px-6" v-if="product">
+	<main class="mx-auto mt-20 lg:mt-32 grid md:grid-cols-2 gap-8 lg:gap-16 px-4 sm:px-6" :class="{'loading': loading}">
 		<div class="flex flex-col md:items-end">
-			<img :src="'/svg/'+product.filename" class="w-full max-w-[400px]" />
+			<div v-if="loading" class="h-full grayout" style="aspect-ratio: 1"></div>
+			<img v-else :src="'/svg/'+product?.filename" class="w-full max-w-[400px]"/>
 		</div>
 
 		<div class="min-h-full w-full  flex flex-col max-w-[400px]">
-			<h1 class="text-2xl sm:text-4xl font-roboto">{{product.title}}</h1>
+			<h1 class="text-2xl sm:text-4xl font-roboto loading:grayout loading:w-full loading:h-12">{{product?.title}}</h1>
 
 			<div class="flex gap-2 mt-2">
-				<RatingStars :rating="product.rating"/>
-				<div class="text-gray-500 ml-2">16 ratings</div>
+				<RatingStars :rating="product?.rating" class="loading:grayout"/>
+				<div class="text-gray-500 ml-2 loading:grayout">16 ratings</div>
 			</div>
 
-			<div class="text-3xl mt-3 mb-4 font-roboto">
-				${{product.price}}
+			<div class="text-3xl mt-3 mb-4 font-roboto loading:grayout loading:w-20">
+				${{product?.price}}
 			</div>
 
-			<div class="mt-auto">
-				{{product.description}}
+			<div class="mt-auto loading:grayout loading:h-24">
+				{{product?.description}}
 			</div>
 
-			<div class="mt-3 mb-6">
-				<span v-for="tag in product.tags" 
+			<div class="mt-3 mb-6 loading:grayout">
+				<span v-for="tag in product?.tags"
 				      class="text-sm p-1 px-2 border- bg-gray-200 text-primary rounded mr-2 text-gray-600">
 					{{tag}}
 				</span>
 			</div>
 
-			<div class="mt-auto text-lg">
+			<div class="mt-auto text-lg loading:grayout loading:w-32">
 				<span class="text-2xl font-bold">5</span> left in stock
 			</div>
 
-			<AddToCartButton :pid="productId" class="sm:w-64 mt-2 text-lg"/>
+			<AddToCartButton :pid="productId" class="sm:w-64 mt-2 text-lg loading:grayout"/>
 		</div>
 	</main>
 
-	<section class="max-w-screen-xl mx-auto mt-32 px-4 sm:px-6" v-if="product">
+	<section class="max-w-screen-xl mx-auto mt-32 px-4 sm:px-6" :class="{'loading': loading}">
 		<h1 class="text-2xl sm:text-3xl">User reviews</h1>
 
 		<div class="flex gap-2 items-center mt-4">
-			<RatingStars :rating="product.rating" class="h-8"/>
-			<div class="text-2xl md:text-3xl font-bold ml-6 font-roboto">
-				{{product.rating}}
+			<RatingStars :rating="product?.rating" class="h-8 loading:grayout"/>
+			<div class="text-2xl md:text-3xl font-bold ml-6 font-roboto loading:grayout loading:w-12 loading:h-8">
+				{{product?.rating}}
 			</div>
-			<div class="ml-4 sm:hidden text-gray-500">
+			<div class="ml-4 sm:hidden text-gray-500 loading:grayout">
 				16 ratings
 			</div>
-			<div class="ml-6 hidden sm:block text-lg text-gray-500">
+			<div class="ml-6 hidden sm:block text-lg text-gray-500 loading:grayout">
 				based on 16 user ratings
 			</div>
 		</div>
 
 		<div class="lg:grid lg:grid-cols-2 gap-6 mt-8 -mx-4 sm:mx-0">
 			<div class="space-y-6">
-				<UserComment username=""/>
-				<UserComment username=""/>
+				<UserComment username="" class="loading:grayout"/>
+				<UserComment username="" class="loading:grayout"/>
 			</div>
 
 			<div class="mt-6 lg:mt-0 space-y-6">
-				<UserComment username=""/>
+				<UserComment username="" class="loading:grayout"/>
 			</div>
 		</div>
 
@@ -90,6 +91,7 @@ const route = useRoute()
 
 const productId = computed(() => route.params.id)
 const product = computed(() => productStore.products[productId.value])
+const loading = computed(() => product.value === undefined)
 
 productStore.fetchProduct(productId.value)
 </script>
