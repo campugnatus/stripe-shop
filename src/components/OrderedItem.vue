36d130d9432fdad3d1c404852bff584587615e3d
Andrey Shevchuk <campugnatus@gmail.com>
2022-03-22 19:33:05 +0300

    ordered item loading and fetching

--- src/components/OrderedItem.vue
+++ src/components/OrderedItem.vue
@@ -1,13 +1,13 @@
 <template>
-	<div class="flex items-center" v-if="product">
-		<router-link :to="'/product/'+item.productId">
-			<img :src="'/svg/'+product?.filename" class="h-20 w-20 border border-gray-300"/>
+	<div class="flex items-center" :class="{loading}">
+		<router-link :to="'/product/'+item.productId" class="loading:grayout">
+			<img :src="'/svg/'+product?.filename" class="h-20 w-20 border loading:border-none border-gray-300"/>
 		</router-link>
 		<div class="ml-4">
-			<router-link :to="'/product/'+item.productId" class="text-lg">
-				{{product.title}}
+			<router-link :to="'/product/'+item.productId" class="text-lg loading:grayout loading:w-10 loading:h-20 loading:after:content-[zhopa]">
+				{{product?.title || "loading"}}
 			</router-link>
-			<div class="flex items-center gap-4">
+			<div class="flex items-center gap-4 loading:grayout">
 				<span class="text-lg text-gray-500 font-mono">
 					${{item.price}}
 				</span>
@@ -23,7 +23,6 @@ import { ref, toRefs, computed } from 'vue'
 import { useProductStore } from '@/stores/products'
 import { XIcon } from '@heroicons/vue/outline'
 
-
 const productStore = useProductStore()
 
 const props = defineProps({
@@ -33,10 +32,9 @@ const props = defineProps({
 	}
 })
 
-// reactivity question: will item change when the prop changes?
 const item = ref(props.item)
+productStore.fetchProduct(item.value.productId)
 const product = computed(() => productStore.products[item.value.productId])
-// TODO: productStore.fetchProduct(item.productId)
-const loading = computed(() => product === undefined)
+const loading = computed(() => product.value === undefined)
 
 </script>
