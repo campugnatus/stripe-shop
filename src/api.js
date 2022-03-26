f2576adde619a1fb8cf7a6b4960b39bb922965a1
Andrey Shevchuk <campugnatus@gmail.com>
2022-03-26 18:29:22 +0300

    cleanup

--- src/api.js
+++ src/api.js
@@ -6,20 +6,73 @@ axios.defaults.baseURL = "http://localhost:3002"
 // for debugging purposes, TODO: turn off in production
 window.axios = axios
 
+
+
+
+export async function fetchProduct (id) {
+	return axios('/products/' + id)
+	.then(response => response.data)
+}
+
+export async function fetchOrder (id) {
+	return axios(`/orders/${id}`)
+	.then(response => response.data)
+}
+
+export async function createOrder ({email, items, price, paymentToken}) {
+	return axios.post("/orders", {email, items, price, paymentToken})
+	.then(response => response.data)
+}
+
+
+
+
+/**
+ * Search
+ */
+
 // in the axios documentation they recommend the new AbortController way of
 // request cancellation. However, I couldn't make it work, so I'm falling
 // back to the old deprecated way
 let CancelToken
 let source
 
+export async function searchProducts (query) {
+	CancelToken = axios.CancelToken
+	source = CancelToken.source()
+
+	return axios('/products/search', {
+		params: query,
+		cancelToken: source.token
+	})
+	.then(response => response.data, error => {
+		if (axios.isCancel(error)) {
+			throw "abort"
+		}
+		else {
+			throw error
+		}
+	})
+}
+
+export function abortSearch () {
+	source.cancel()
+}
 
 
-const wshost = "ws://localhost:3002"
+
+
+
+/**
+ * Pub/Sub
+ */
 
 // websocket for the pubsub
 let ws
 
-// we keep track of our subscriptions for the sole purpose of avoiding
+const wshost = "ws://localhost:3002"
+
+// we keep track of our subscriptions for the sole purpose of avoiding having
 // multiple subscriptions to the same object... which, theoretically, might
 // not be such a bad thing, after all: different places might want to
 // subscribe to the same thing for different reasons, nothing wrong with
@@ -27,78 +80,31 @@ let ws
 // there a way?
 let subs = {}
 
+export async function subscribe (bucket, id, callback) {
+	await ensureSocket()
 
-export default {
-	async fetchProduct (id) {
-		return axios('/products/' + id)
-		       .then(response => response.data)
-	},
+	const subName = `${bucket}/${id}`
 
-	async searchProducts (query) {
-		CancelToken = axios.CancelToken
-		source = CancelToken.source()
+	if (subs[subName]) {
+		// already subscribed
+		// TODO: is there a better place for this logic?
+		return
+	}
 
-		return axios('/products/search', {
-			params: query,
-			cancelToken: source.token
-		})
-		.then(response => response.data, error => {
-			if (axios.isCancel(error)) {
-				throw "abort"
-			}
-			else {
-				throw error
-			}
-		})
-	},
-	
-	abortSearch () {
-		source.cancel()
-	},
+	subs[subName] = callback
 
-	async login () {
-		return axios({
-			url: "/login",
+	ws.send(JSON.stringify({tag: "subscribe", bucket, id}))
 
-		})
-		.then(response => response.data)
-	},
-
-	async fetchOrder (id) {
-		return axios(`/orders/${id}`)
-		       .then(response => response.data)
-	},
-
-	async createOrder ({email, items, price, paymentToken}) {
-		return axios.post("/orders", {email, items, price, paymentToken})
-					.then(response => response.data)
-	},
-
-	async subscribe (bucket, id, callback) {
-		await ensureSocket()
-
-		const subName = `${bucket}/${id}`
-
-		if (subs[subName]) {
-			// already subscribed
-			// TODO: is there a better place for this logic?
-			return
+	ws.addEventListener('message', ({data}) => {
+		let msg = JSON.parse(data)
+		if (msg.bucket === bucket && msg.id === id) {
+			callback(msg.object)
 		}
-
-		subs[subName] = callback
-
-		ws.send(JSON.stringify({tag: "subscribe", bucket, id}))
-
-		ws.addEventListener('message', ({data}) => {
-			let msg = JSON.parse(data)
-			if (msg.bucket === bucket && msg.id === id) {
-				callback(msg.object)
-			}
-		})
-	},
+	})
 }
 
-async function ensureSocket () {
+
+export async function ensureSocket () {
 	if (ws?.readyState === 1 /* OPEN */)
 		return Promise.resolve()
 
@@ -122,4 +128,18 @@ async function ensureSocket () {
 		// })
 	})
 
+}
+
+
+
+
+
+export default {
+	searchProducts,
+	abortSearch,
+	subscribe,
+	ensureSocket,
+	fetchProduct,
+	fetchOrder,
+	createOrder,
 }
