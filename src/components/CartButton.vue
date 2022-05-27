c0872e3218980fa435f9955fd5474465a8366860
Andrey Shevchuk <campugnatus@gmail.com>
2022-05-27 13:38:25 +0300

    shopping cart button icon

--- src/components/CartButton.vue
+++ src/components/CartButton.vue
@@ -1,10 +1,7 @@
 <template>
 	<router-link class="p-2" to="/cart">
 		<div class="flex justify-center" :class="{'grayout': cart.loading}">
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-10 lg:h-11 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				<!-- I would use a component from the @heroicons npm package, but I need to set the stroke-width -->
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-			</svg>
+			<ShoppingCartIcon class="h-10 lg:h-11 text-gray-600 stroke-1"/>
 		</div>
 		<div class="lg:mt-2 text-center w-[4em]" :class="{'grayout': cart.loading}">
 			<b>{{cart.totalAmount}}</b> {{ cart.totalAmount === 1 ? "item" : "items"}}
@@ -14,6 +11,7 @@
 
 <script setup>
 import { useCartStore } from '@/stores/cart'
+import { ShoppingCartIcon } from '@heroicons/vue/outline'
 
 const cart = useCartStore()
 </script>
