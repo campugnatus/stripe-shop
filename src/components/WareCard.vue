e10d5bacfcc9e8c24d250ae1712f50ff7963f7b5
Andrey Shevchuk <campugnatus@gmail.com>
2022-06-05 16:58:19 +0300

    warecard number of reviews

--- src/components/WareCard.vue
+++ src/components/WareCard.vue
@@ -13,15 +13,11 @@
         </h1>
       </router-link>
 
-      <div class="flex mt-1 mb-1 justify-between">
-        <div class="flex gap-1 loading:grayout">
-          <RatingStars v-if="!loading" :rating="product?.rating" class="h-5 space-x-1.5" :title="product?.rating"/>
-        </div>
-        <div class="flex gap-2 loading:grayout">
-          12 
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8 10H8.01H8ZM12 10H12.01H12ZM16 10H16.01H16ZM9 16H5C4.46957 16 3.96086 15.7893 3.58579 15.4142C3.21071 15.0391 3 14.5304 3 14V6C3 5.46957 3.21071 4.96086 3.58579 4.58579C3.96086 4.21071 4.46957 4 5 4H19C19.5304 4 20.0391 4.21071 20.4142 4.58579C20.7893 4.96086 21 5.46957 21 6V14C21 14.5304 20.7893 15.0391 20.4142 15.4142C20.0391 15.7893 19.5304 16 19 16H14L9 21V16Z" stroke="#24282B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
+      <div class="mt-1 mb-1 flex items-center">
+        <RatingStars :rating="product?.rating" class="h-5 loading:grayout" :title="product?.rating"/>
+        <span class="ml-auto loading:grayout loading:w-6">{{product?.nreviews || 0}}</span>
+        <div class="loading:grayout">
+          <ChatAltIcon class="ml-1 h-6 text-gray-700"/>
         </div>
       </div>
 
@@ -44,6 +40,9 @@
 <script setup>
 import RatingStars from '@/components/RatingStars.vue'
 import AddToCartButton from '@/components/AddToCartButton.vue'
+// import {  } from '@heroicons/vue/solid'
+import { ChatAltIcon } from '@heroicons/vue/outline'
+
 
 import { useProductStore } from '@/stores/products'
 import { useCartStore } from '@/stores/cart'
