c484de0b3c43c5c3c8f63c59c0c69358c3287d68
Andrey Shevchuk <campugnatus@gmail.com>
2022-06-26 10:21:12 +0300

    remvoe the search button from the front page

--- src/views/Front.vue
+++ src/views/Front.vue
@@ -37,16 +37,6 @@
         </section>
 
         <section class="space-x-4 md:space-x-7 flex items-center">
-
-          <button class="lg:p-2 text-sm lg:text-base hidden sm:inline">
-            <div class="flex justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 lg:h-11 lg:w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </div>
-            <div class="pt-2">search</div>
-          </button>
-
           <UserButton/>
           <CartButton/>
         </section>
